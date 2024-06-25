41904bd257342fc4bb66687c05b169a7fb944e47
Ariel Nunes da Silva <arielnds274@gmail.com>
2024-06-25 14:56:10 -0300

    incrementing cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,11 @@
 const express = require('express');     // utilizar o express.js
 const routes = require('./routes');
-
+const cors = require('cors');
 
 const app = express();          // guarda 'express()' na variável app
 require('./config/db_config.js');
 
+app.use(cors());
 app.use(express.json());       
 app.use(routes);
 
