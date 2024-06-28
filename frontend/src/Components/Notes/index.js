d0f441a60fac341b4fe24ee713e743f5ae97181d
Ariel Nunes da Silva <arielnds274@gmail.com>
2024-06-28 10:46:36 -0300

    refactor: creating an archive for Notes function

---
+++ frontend/src/Components/Notes/index.js
@@ -0,0 +1,79 @@
+import React from 'react';
+
+function Notes() {
+    return (
+<>
+    <li className='notepad-info'>
+        <div>
+          <strong>Fazer Compras</strong>
+            <div>
+                x
+            </div>
+        </div>
+        <textarea></textarea>
+    </li>
+      
+    <li className='notepad-info'>
+        <div>
+          <strong>Fazer Compras</strong>
+            <div>
+                x
+            </div>
+        </div>
+        <textarea></textarea>
+    </li>
+
+    <li className='notepad-info'>
+        <div>
+          <strong>Fazer Compras</strong>
+            <div>
+                x
+            </div>
+        </div>
+        <textarea></textarea>
+    </li>
+
+    <li className='notepad-info'>
+        <div>
+          <strong>Fazer Compras</strong>
+            <div>
+                x
+            </div>
+        </div>
+        <textarea></textarea>
+    </li>
+
+    <li className='notepad-info'>
+        <div>
+          <strong>Fazer Compras</strong>
+            <div>
+                x
+            </div>
+        </div>
+        <textarea></textarea>
+    </li>
+
+    <li className='notepad-info'>
+        <div>
+          <strong>Fazer Compras</strong>
+            <div>
+                x
+            </div>
+        </div>
+        <textarea></textarea>
+    </li>
+
+    <li className='notepad-info'>
+        <div>
+          <strong>Fazer Compras</strong>
+            <div>
+                x
+            </div>
+        </div>
+        <textarea></textarea>
+    </li>
+</>
+    )
+}
+
+export default Notes;
